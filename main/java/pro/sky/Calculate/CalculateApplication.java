5bac496392b093cf5f26f9edc07c44d4e1d22e07
olga-kri <krivenkoolga1987@gmail.com>
2023-12-04 01:10:49 +0500

    Initial commit

---
+++ main/java/pro/sky/Calculate/CalculateApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Calculate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateApplication.class, args);
+	}
+
+}
